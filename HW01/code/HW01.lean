00912ad597c570937e940aae09f126826d74e264
Lucas Tassis <lucaswfitassis@gmail.com>
2024-09-09 15:14:25 -0400

    HW01: update problem 2 Lean solution

--- HW01/code/HW01.lean
+++ HW01/code/HW01.lean
@@ -18,3 +18,15 @@ example {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 
     _ = (a - 3) ^ 2 + 5 * (a - 3) + 9 := by ring
     _ = (2 * b) ^ 2 + 5 * (2 * b) + 9 := by rw [h1]
     _ = 4 * b ^ 2 + 10 * b + 9 := by ring
+
+example {a b c : ℝ} (h1 : a + 2 * b + 3 * c = 7) (h2 : b + 2 * c = 3)
+    (h3 : c = 1) : a = 2 :=
+  calc
+    a = (a + 2 * b + 3 * c) - 2 * b - 3 * c := by ring
+    _ = 7 - 2 * b - 3 * c := by rw [h1]
+    _ = 7 - (b + 2 * c) - b - c := by ring
+    _ = 7 - 3 - b - 1 := by rw [h2, h3]
+    _ = - b - 2 * c + 2 * c + 3 := by ring
+    _ = - (b + 2 * c) + 2 * c + 3 := by ring
+    _ = - 3 + 2 * 1 + 3 := by rw [h2, h3]
+    _ = 2 := by ring
