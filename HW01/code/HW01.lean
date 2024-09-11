f9b0b0e41243e9079924a0a8d38a4069cebfddb8
Lucas Tassis <lucaswfitassis@gmail.com>
2024-09-11 13:13:55 -0400

    HW01: update lean template

--- HW01/code/HW01.lean
+++ HW01/code/HW01.lean
@@ -1,16 +1,19 @@
 import Mathlib.Data.Real.Basic
 import Library.Basic
 
--- Example 1.3.4
-example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 :=
+math2001_init
+
+/-! # CS 511 Homework 1 -/
+
+-- Exercise 3:  MoP Example 1.3.4
+example {w : ℚ} (h1 : 3 * w + 1 = 4) : w = 1 := by
   calc
     w = ((3 * w + 1) / 3) - (1 / 3) := by ring
     _ = (4 / 3) - 1 / 3 := by rw [h1]
     _ = 1 := by ring
 
-
--- Example 1.3.9
-example {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 9 :=
+-- Exercise 4:  MoP Example 1.3.9
+example {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 9 := by
   calc
     a ^ 2 - a + 3 = ((a - 3) ^ 2 + 6 * a - 9) - a + 3 := by ring
     _ = (a - 3) ^ 2 + 5 * a - 6 := by ring
@@ -19,9 +22,9 @@ example {a b : ℚ} (h1 : a - 3 = 2 * b) : a ^ 2 - a + 3 = 4 * b ^ 2 + 10 * b + 
     _ = (2 * b) ^ 2 + 5 * (2 * b) + 9 := by rw [h1]
     _ = 4 * b ^ 2 + 10 * b + 9 := by ring
 
--- Section 1.3.11 - Exercise 7
+-- Problem 2:  MoP Exercise 1.3.11
 example {a b c : ℝ} (h1 : a + 2 * b + 3 * c = 7) (h2 : b + 2 * c = 3)
-    (h3 : c = 1) : a = 2 :=
+    (h3 : c = 1) : a = 2 := by
   calc
     a = (a + 2 * b + 3 * c) - 2 * b - 3 * c := by ring
     _ = 7 - 2 * b - 3 * c := by rw [h1]
